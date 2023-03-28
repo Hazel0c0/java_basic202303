aa147de4ab4488c1771868a797951e78f6d5eedc
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-03-28 12:41:08 +0900

    배열 push, pop 공부하기

--- src/day02/ArrayBasic.java
+++ src/day02/ArrayBasic.java
@@ -6,9 +6,17 @@ public class ArrayBasic {
   자바의 배열 immutable(불변성)
    */
 
-    int [] arr = new int[5];
+    int[] arr = new int[5];
+    arr[0] = 1;
+    arr[1] = 1;
+    arr[2] = 1;
+    arr[3] = 1;
+    arr[4] = 1;
+
+
+/*
     //배열 for문
-    for (int i = 0; i < ; i++) { // fori
+    for (int i = 0; i < 5 ; i++) { // fori
 
     }
     // foreach문
@@ -18,7 +26,8 @@ public class ArrayBasic {
     for (String arg : args) {  // iter
 
     }
+*/
+
 
   }
-
 }
