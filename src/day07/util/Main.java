3810bef3704d9e15888fdf03d32b820f0646741b
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-04-05 11:26:49 +0900

    0405 day07 스태틱_ 보기

--- src/day07/util/Main.java
+++
@@ -1,9 +0,0 @@
-package day07.util;
-
-import static day07.util.Utility.*;
-
-public class Main {
-  public static void main(String[] args) {
-    input("메인이다");
-  }
-}
--- src/day07/util/Utility.java
+++ src/day07/util/Utility.java
@@ -8,6 +8,8 @@ public class Utility {
 
   private static Scanner sc = new Scanner(System.in);
 
+  private Utility() {}
+
   // 구분선을 출력하는 기능
   public static void makeLine() {
     System.out.println("========================");
@@ -19,10 +21,10 @@ public class Utility {
     return sc.nextLine();
   }
 
-  public static void main(String[] args) {
-    makeLine();
-    input("메롱");
-    input("가가가");
-  }
+//  public static void main(String[] args) {
+//    makeLine();
+//    input("메롱");
+//    input("가가가");
+//  }
 
 }
