ebd9a3303a39366d8aa739dccc0545d7b24e06f2
Hazel <secretwish5@naver.com>
2023-04-08 17:53:37 +0900

    ListSearchQuiz 완성

--- src/day10/collection/list/ListSearchQuiz.java
+++ src/day10/collection/list/ListSearchQuiz.java
@@ -1,55 +1,58 @@
 package day10.collection.list;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+
+import static day07.util.Utility.input;
 
 public class ListSearchQuiz {
 
   public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
+    ArrayList<String> tvxq = new ArrayList<>(
+        Arrays.asList("영웅재중", "최강창민", "시아준수", "믹키유천", "유노윤호")
+    );
 
-    String[] tvxq = {"영웅재중", "최강창민", "시아준수", "믹키유천", "유노윤호"};
+    System.out.println(tvxq);
 
-    System.out.println("* 변경 전 정보: " + Arrays.toString(tvxq));
-
-    // 1. 수정 타겟의 이름을 입력받는다.
-    // 2. 해당 이름이 있는지 탐색해본다.
-    // 3. 탐색에 성공하면 해당 이름의 인덱스를 알아온다.
-    // 4. 탐색에 실패하면 실패한 증거를 확보한다.
-    // 5. 성공했을 시 수정을 원하는 새로운 이름을 입력받는다.
-    // 6. 찾은 인덱스를 통해 새로운 이름으로 수정한다.
-    // 7. 위 내용을 수정이 정확히 완료될때까지 반복한다.
 
     while (true) {
+      // 1. 수정 타겟의 이름을 입력받는다.
       System.out.println("- 수정할 멤버의 이름을 입력하세요.");
-      System.out.printf(">> ");
-      String targetName = sc.nextLine();
+      String targetName = input(">> ");
 
-      //탐색 알고리즘
-      int index = -1;
-      for (int i = 0; i < tvxq.length; i++) {
-        if (targetName.equals(tvxq[i])) {
-          index = i;
-          break;
-        }
+
+      // 2. 해당 이름이 있는지 탐색해본다.
+      boolean isName = tvxq.contains(targetName);
+      System.out.println(targetName + " 있나요? " +
+          isName);
+
+      // 4. 탐색에 실패하면 실패한 증거를 확보한다.
+      try {
+        if (!isName)
+          throw new Exception();
+
+      } catch (Exception e) {
+        System.out.println("이름을 다시 입력해랏!");
+        e.printStackTrace();
       }
 
-      //수정 여부 판단
-      if (index != -1) {
-        System.out.printf("%s의 별명을 변경합니다.\n", targetName);
-        System.out.printf(">> ");
-        String newName = sc.nextLine();
-        tvxq[index] = newName;
-        System.out.println("변경 완료!!");
-        System.out.println("* 변경 후 정보: " + Arrays.toString(tvxq));
+      // 3. 탐색에 성공하면 해당 이름의 인덱스를 알아온다.
+      if (isName) {
+        int index = tvxq.indexOf(targetName);
+
+        // 5. 성공했을 시 수정을 원하는 새로운 이름을 입력받는다.
+        String newName = input("변경할 이름을 입력해주세요\n>> ");
+
+        // 6. 찾은 인덱스를 통해 새로운 이름으로 수정한다.
+        tvxq.set(index, newName);
+
+
+        // 7. 위 내용을 수정이 정확히 완료될때까지 반복한다.
         break;
-      } else {
-        System.out.printf("%s은(는) 없는 이름입니다.\n", targetName);
       }
-    } //end while
-    sc.close();
-  } // end main
-
-} // end class
+    }
+    System.out.println(tvxq);
+  }
+}
 
