9b00c13ba514d96d830d8eae5830192a36a1ef32
Hazel <secretwish5@naver.com>
2023-04-10 02:50:55 +0900

    0410

---
+++ src/day07/static_/Static_ex.java
@@ -0,0 +1,11 @@
+package day07.static_;
+
+public class Static_ex {
+  static int n;
+  static String str;
+
+  public static void main(String[] args) {
+    System.out.println(n);
+    System.out.println(str);
+  }
+}
