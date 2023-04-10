84c4061bceba39f36325671e63a2039f8f5f8fdb
Hazel <secretwish5@naver.com>
2023-04-10 12:49:43 +0900

    0410

---
+++ src/day10/collection/map/Main.java
@@ -0,0 +1,4 @@
+package day10.collection.map;
+
+public class Main {
+}
---
+++ src/day11/api/obj/Pen.java
@@ -0,0 +1,16 @@
+package day11.api.obj;
+
+import java.util.List;
+
+public class Pen extends Object {
+
+  long serial; // 시리얼 번호
+  String color; // 색상
+  int price; // 가격
+
+  public Pen(long serial, String color, int price) {
+    this.serial = serial;
+    this.color = color;
+    this.price = price;
+  }
+}
