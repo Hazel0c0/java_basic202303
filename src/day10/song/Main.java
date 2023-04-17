af5591f62aab7df1d07c849667388c5abb8aae0d
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-04-17 21:04:26 +0900

    0417 map, API

---
+++ src/day10/song/Main.java
@@ -0,0 +1,11 @@
+package day10.song;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 세이브파일 자동로드
+        ArtistRepository.loadFile();
+
+        ArtistView.start();
+    }
+}
